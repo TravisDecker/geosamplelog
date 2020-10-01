af27b3977fbe54e0e9ee932cadd099ec72261728
Travis Decker <travisdecker@Traviss-MBP.hsd1.nm.comcast.net>
2020-10-01 11:49:47 -0600

    Initial commit

---
+++ src/main/java/com/straylense/geosamplelog/GeosamplelogApplication.java
@@ -0,0 +1,13 @@
+package com.straylense.geosamplelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeosamplelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeosamplelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/straylense/geosamplelog/GeosamplelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.straylense.geosamplelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeosamplelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
